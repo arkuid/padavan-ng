d9d471ac26aec2f7ac21d0811eef967a6ed16ff5
nil <cryptonil@ya.ru>
2025-11-12 16:35:06 +0700

    wireguard: added bogon networks to exclude

--- trunk/user/wireguard/wgc.sh
+++ trunk/user/wireguard/wgc.sh
@@ -499,6 +499,13 @@ start_fw()
 -A vpnc_wireguard -d 0.0.0.0/8 -j RETURN
 -A vpnc_wireguard -d 127.0.0.0/8 -j RETURN
 -A vpnc_wireguard -d 169.254.0.0/16 -j RETURN
+-A vpnc_wireguard -d 100.64.0.0/10 -j RETURN
+-A vpnc_wireguard -d 198.18.0.0/15 -j RETURN
+-A vpnc_wireguard -d 192.88.99.0/24 -j RETURN
+-A vpnc_wireguard -d 192.0.0.0/24 -j RETURN
+-A vpnc_wireguard -d 192.0.2.0/24 -j RETURN
+-A vpnc_wireguard -d 198.51.100.0/24 -j RETURN
+-A vpnc_wireguard -d 203.0.113.0/24 -j RETURN
 -A vpnc_wireguard -d 224.0.0.0/4 -j RETURN
 -A vpnc_wireguard -d 240.0.0.0/4 -j RETURN
 -A vpnc_wireguard -p udp --dport 53 -j RETURN
